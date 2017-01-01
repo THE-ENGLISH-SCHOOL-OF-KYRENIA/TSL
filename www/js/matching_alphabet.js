5145128b1625948b2df7eeec04b830c6125c5736
akzo <anarkazimov98@hotmail.com>
2017-01-01 22:05:09 +0300

    Made the letters in the alphabet section automatically changing

--- www/js/matching_alphabet.js
+++ www/js/matching_alphabet.js
@@ -1,40 +1,71 @@
-//This Javascript is responsible for matching letters with gestures images in alphabet pages
+//This Javascript is responsible for matching letters with gesture images on alphabet pages
 $(document).ready(function(){
     $(".letters a").addClass('inactive');
-    $("#a_ltr").addClass('selected');
-            $( "a" ).click(function() {  
-                $("a").removeClass('selected');                
-                $( this ).addClass('selected');
+    $("#default").addClass('selected');
+    $( "a" ).click(function() { 
+      //highlighting letters
+      $("a").removeClass('selected');                
+      $( this ).addClass('selected');
+        
+      //Deleting used intervals
+      try{
+        window.clearInterval(interval);
+      } catch(err){}
+      
                 
-                //manipulating with images
-                var letter = $(this).text();
-                if(letter == "Ö" ){ 
-                    $("a").addClass("wait_click");// so that user doesn't click another letter before  gesture wasn't showed fully                    
-                    $("#sign").attr("src","../images/gestures/O-1.jpg")
-                    setTimeout(function(){                        
-                        $("#sign").attr("src","../images/gestures/O-2.jpg") 
-                        $("a").removeClass("wait_click");
-                    }, 1000);                       
-                                      
-                    
-                }
+      //manipulating images
+      var letter = $(this).text();
+        
+      if(letter == "Ö" ){             
+        function turkishO(){ //For double and triple sign words we need intervals
+          link = $("#sign").attr("src");
+          if(link!="../images/gestures/O-1.jpg"){ 
+            $("#sign").attr("src","../images/gestures/O-1.jpg");
+          }
+          else{
+            $("#sign").attr("src","../images/gestures/O-2.jpg")
+          }
+          window.clearInterval(window.interval);
+          window.interval = setInterval(turkishO, 1000);//let's increase interval a bit after the first run   
+       }
+       window.interval = setInterval(turkishO, 300);
+       }
+      
+     else if(letter == "Y"){
+       function Y(){ //For double and triple sign words we need intervals
+         link = $("#sign").attr("src");
+         if(link!="../images/gestures/Y.jpg"){
+           $("#sign").attr("src","../images/gestures/Y.jpg");
+         }
+         else{
+           $("#sign").attr("src","../images/gestures/Y2.jpg")
+         }
+         window.clearInterval(window.interval);
+         window.interval = setInterval(Y, 1000);//let's increase interval a bit after the first run   
+       }
+       window.interval = setInterval(Y, 300);
+     }
                 
-                else if(letter == "Y"){
-                    $("a").addClass("wait_click");// so that user doesn't click another letter before  gesture wasn't showed fully
-                    $("#sign").attr("src","../images/gestures/Y.jpg")
-                    setTimeout(function(){                        
-                        $("#sign").attr("src","../images/gestures/Y2.jpg") 
-                        $("a").removeClass("wait_click");
-                    }, 1000);      
-                }
-                
-                else if(letter == "Ü"){
-                    $("a").addClass("wait_click");// so that user doesn't click another letter before  gesture wasn't showed fully
+     else if(letter == "Ü"){
+       function turkishU(){ //For double and triple sign words we need intervals
+         link = $("#sign").attr("src");
+         if(link!="../images/gestures/U-1.jpg"){
+           $("#sign").attr("src","../images/gestures/U-1.jpg");
+         }
+         else{
+           $("#sign").attr("src","../images/gestures/U-2.jpg")
+         }
+         window.clearInterval(window.interval);
+         window.interval = setInterval(turkishU, 1000);//let's increase interval a bit after the first run   
+       }
+       window.interval = setInterval(turkishU, 300);
+        /*
+       $("a").addClass("wait_click");// so that user doesn't click another letter before  gesture wasn't showed fully
                     $("#sign").attr("src","../images/gestures/U-1.jpg")
                     setTimeout(function(){                        
                         $("#sign").attr("src","../images/gestures/U-2.jpg") 
                         $("a").removeClass("wait_click");
-                    }, 1000); 
+                    }, 1000); */
                 }
     
                 else if(letter == "Ç"){
