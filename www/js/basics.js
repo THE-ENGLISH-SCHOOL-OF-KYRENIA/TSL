45d37990cc1efffc80b950a2cc4ed790b0f3d187
kinetickaan <kaanibrahim1999@gmail.com>
2017-02-07 12:12:50 +0300

    Added some comments to a js script

--- www/js/basics.js
+++ www/js/basics.js
@@ -3,13 +3,14 @@ $(document).ready(function(){
   
   setInterval(function(){
     
+	//landscape
     if(window.innerHeight < window.innerWidth){
       $('#greeting').css('margin-left', '10%');
       $('#weather').css('margin-left', '10%');
       $('#colours').css('margin-left', '70%');
       $('#directions').css('margin-left', '70%');
     }
-    
+    //portrait
     else{
       $('#greeting').css('margin-left', '9%');
       $('#weather').css('margin-left', '9%');
