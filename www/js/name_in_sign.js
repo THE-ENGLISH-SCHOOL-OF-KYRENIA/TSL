ecf7f3ef3924a47a089f83347de6258d15e5cc69
kinetickaan <kaanibrahim1999@gmail.com>
2017-02-20 16:16:30 +0300

    incomplete, small tweak
    only allowing users to use letter in their name, also, it could be
    modified so that the user cannot use swear words in their name

--- www/js/name_in_sign.js
+++ www/js/name_in_sign.js
@@ -1,11 +1,9 @@
 //Javascript showing name in sign language
 function show_name(name){
-	
       for (var i = 0, len = name.length; i < len; i++) {
 		  
-		 localStorage.setItem('name', name);
-		  
-        //these three lines below cause every iteration to run with a delay which 2.5 seconds in this case
+		 localStorage.setItem('name', name);  
+        //these three lines below cause every iteration to run with a delay which is 2.5 seconds in this case
         window.setTimeout(
           (function (i){ 
             return function() {
@@ -58,7 +56,11 @@ function show_name(name){
               }
                                 
               else if( name[i] == " "){}
-                                
+			  
+			  else if( name[i].toUpperCase() != "A" or "B" or "C" or "Ç" or "D" or "E" or "F" or "G" or "Ğ" or "H" or "I" or "İ" or "J" or "K" or "L" or "M" "N" "O" or "Ö" or "P" or "R" or "S" or "Ş" or "T" or "U" or "Ü" or "V" or "Y" or "Z"){
+				 $("#warning_message").html("please use lettters for your name")
+			  }
+			  
               else{
                 $("#sign").attr("src","../images/gestures/" + name[i].toUpperCase() +".jpg"); 
               }                   
@@ -73,7 +75,7 @@ function show_name(name){
                   location.reload();
                 },2000);
               }
-                                                 
+                                                
             }
         })(i),i * 4000)
     }
